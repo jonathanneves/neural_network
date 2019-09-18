7c788e3eb0d1e0130885749b9f7c2fd66ee3a883
Junior <vilmarmendesjunior@hotmail.com>
2019-09-18 16:56:27 -0300

    Ajustes nos comentários

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,8 +10,8 @@ import core.NetworkTrainer;
 public class Main {	
 	/**
 	 * 	
-	 * @autor Jonathan Neves, Vilmar Mendes Jr, Adriano Karas
-	 * @instrucao
+	 * @authors Jonathan Neves, Vilmar Mendes Jr, Adriano Karas
+	 * @instructions
 	 * Como realizar o treinamento:
 	 * 1º Crie arquivos txt na pasta Letters com todos os inputs e na última linha do txt defina a saída desejada.
 	 * Os inputs devem seguir o padrão: '.' = -1 '#' = 1  'etc' = 0;
@@ -22,8 +22,7 @@ public class Main {
 	 * 2º Crie um arquivo chamado TESTE.txt, coloque apenas os inputs para Rede Neural classificar no teste
 	 * OBS.: Deve ter o mesmo número de inputs que os outros arquivos.
 	 * 
-	 * Como configurar o algorit
-	 * mo:
+	 * Como configurar o algoritmo:
 	 * -Na classe Variables você pode definir o número de LAYER j desejado. Recomendado ser igual ao número de inputs.
 	 * -Na Classse Constants você pode definir o número de epócas em EPOCHS. É o número de vezes que o algoritmo vai realizar o treinamento antes de parar.
 	 * 
